e4e415ca6647e994b7c5238f98b38ec9c67fa0fc
oleg <hnapovskiy20@gmail.com>
2019-09-23 14:54:24 +0300

    Updates

---
+++ precache-manifest.45713b8e19c46b63b4d90ec6d84ed905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ee98427ac143cdd157a0a44548c7ae",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "f0c4b1ced973361e57f0",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "6434737b2c4ee92ce4c9",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.7c05b8cb.chunk.css"
+  },
+  {
+    "revision": "f0c4b1ced973361e57f0",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.51bac879.chunk.js"
+  },
+  {
+    "revision": "6434737b2c4ee92ce4c9",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.9224365b.chunk.js"
+  },
+  {
+    "revision": "c65525d2042a78ef186a",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.94f51c63.js"
+  }
+]);
